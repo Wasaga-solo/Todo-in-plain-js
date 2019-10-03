4a2d45a6aef3dd8a41462c3375ea6cb0d26a804d
Wasaga Elisha <wasagaelisha8@gmail.com>
2019-10-03 14:55:39 +0300

    My todo in pure js

--- README.md
+++
@@ -1,1 +0,0 @@
-# Todo-in-plain-js
---
+++ main.js
@@ -0,0 +1,41 @@
+var counter = 0;
+var removeSVG='<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="408.483px" height="408.483px" viewBox="0 0 408.483 408.483" style="enable-background:new 0 0 408.483 408.483;"xml:space="preserve"> <path class="fill" d="M87.748,388.784c0.461,11.01,9.521,19.699,20.539,19.699h191.911c11.018,0,20.078-8.689,20.539-19.699l13.705-289.316 H74.043L87.748,388.784z M247.655,171.329c0-4.61,3.738-8.349,8.35-8.349h13.355c4.609,0,8.35,3.738,8.35,8.349v165.293c0,4.611-3.738,8.349-8.35,8.349h-13.355c-4.61,0-8.35-3.736-8.35-8.349V171.329z M189.216,171.329c0-4.61,3.738-8.349,8.349-8.349h13.355c4.609,0,8.349,3.738,8.349,8.349v165.293c0,4.611-3.737,8.349-8.349,8.349h-13.355c-4.61,0-8.349-3.736-8.349-8.349V171.329L189.216,171.329z M130.775,171.329c0-4.61,3.738-8.349,8.349-8.349h13.356 c4.61,0,8.349,3.738,8.349,8.349v165.293c0,4.611-3.738,8.349-8.349,8.349h-13.356c-4.61,0-8.349-3.736-8.349-8.349V171.329z"/><path class="fill" d="M343.567,21.043h-88.535V4.305c0-2.377-1.927-4.305-4.305-4.305h-92.971c-2.377,0-4.304,1.928-4.304,4.305v16.737H64.916c-7.125,0-12.9,5.776-12.9,12.901V74.47h304.451V33.944C356.467,26.819,350.692,21.043,343.567,21.043z"/> </svg>';
+
+var completeSVG='<svg height="512pt" viewBox="0 0 512 512" width="512pt" xmlns="http://www.w3.org/2000/svg"><path class="fill" d="m256 0c-141.164062 0-256 114.835938-256 256s114.835938 256 256 256 256-114.835938 256-256-114.835938-256-256-256zm0 0" fill="#2196f3"/><path d="m385.75 201.75-138.667969 138.664062c-4.160156 4.160157-9.621093 6.253907-15.082031 6.253907s-10.921875-2.09375-15.082031-6.253907l-69.332031-69.332031c-8.34375-8.339843-8.34375-21.824219 0-30.164062 8.339843-8.34375 21.820312-8.34375 30.164062 0l54.25 54.25 123.585938-123.582031c8.339843-8.34375 21.820312-8.34375 30.164062 0 8.339844 8.339843 8.339844 21.820312 0 30.164062zm0 0" fill="#fafafa"/></svg>';
+
+document.getElementById('add').addEventListener('click',function(){
+  var value=document.getElementById('item').value;
+  if(value){ 
+    addItemTodo(value);
+    document.getElementById('item').value='';
+  
+  }
+      
+});
+
+// ADD A NEW ITEM TO THE LIST
+function addItemTodo(text){
+  var list=document.getElementById('todo');
+
+  var item=document.createElement('li');
+  item.innerText=text;
+
+  var buttons=document.createElement('div');
+  buttons.classList.add('buttons');
+
+  var remove=document.createElement('button');
+  remove.classList.add('remove');
+  remove.innerHTML=removeSVG;
+
+  var complete=document.createElement('button');
+  complete.classList.add('complete');
+  complete.innerHTML=completeSVG;
+
+  buttons.appendChild(remove);
+  buttons.appendChild(complete);
+  item.appendChild(buttons);
+
+  list.insertBefore(item,list.childNodes[0]); 
+}
+
+
